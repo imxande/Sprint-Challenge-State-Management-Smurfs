7402a1e795c5c1e2441a955d14297096d3c48b8f
imxande <alerodriguezlarosa@gmail.com>
2019-12-13 16:03:08 -0500

    good I am hitting the API and captured the response

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,9 +1,26 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
-import {SmurfContext} from '../contexts/SmurfContext';
+import {SmurfContext} from '../contexts/SmurfContext'; // context object to use the goodies that comes with it (Provider, value)
+import axios from 'axios'; // using axios to get response
 
 function App () {
+
+  //useState Hook to set state
   const [smurfs, setSmurfs] = useState();
+
+  //useEffect hook to get api request goin
+  useEffect(() =>{
+    axios.get( `http://localhost:3333/smurfs`)
+    .then(response =>{
+      console.log(response.data)
+      setSmurfs(response.data)
+
+    }) // end of then
+    .catch(error => 
+      {console.log(error)
+    })// end of catch
+
+  },[])// end of hook
 
   return(
     <div className="App">
