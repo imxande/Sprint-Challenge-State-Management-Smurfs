cc3aa38a13dd70ed2ac46aba3b5d3d39c66e2473
imxande <alerodriguezlarosa@gmail.com>
2019-12-13 16:10:27 -0500

    wrapped Provider around my component and pass state to the value prop

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -8,7 +8,7 @@ function App () {
   //useState Hook to set state
   const [smurfs, setSmurfs] = useState();
 
-  //useEffect hook to get api request goin
+  //useEffect hook to get API request going
   useEffect(() =>{
     axios.get( `http://localhost:3333/smurfs`)
     .then(response =>{
@@ -23,12 +23,17 @@ function App () {
   },[])// end of hook
 
   return(
-    <div className="App">
-      <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
+    // SmurfContext Provider needs to wrap our component and set the value so I can then pass it later to other components
+    <SmurfContext.Provider values= {{smurfs, setSmurfs}}>
+      <div className="App">
+        <h1>SMURFS! 2.0 W/ ContextAPI</h1>
+        <div>Welcome to your state management version of Smurfs!</div>
+        <div>Start inside of your `src/index.js` file!</div>
+        <div>Have fun!</div>
+        <div>Component Goes Here!</div>
   </div>
+    </SmurfContext.Provider>
+    
   )// end of return
 }// end of function APP
 
