6952bc61481b0231c98bc0ce895e771862d0c8b2
imxande <alerodriguezlarosa@gmail.com>
2019-12-13 15:41:06 -0500

    imported SmurfContext

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
+import {SmurfContext} from '../src/contexts/smurfContext';
+
 class App extends Component {
   render() {
     return (
